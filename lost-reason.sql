ccca8855e2053b28cb7eb616636f646c7d03ba5d
pedro <pedrocortes.pc@outlook.com>
2021-07-12 19:07:44 -0300

    Update lost-reason.sql

--- lost-reason.sql
+++ lost-reason.sql
@@ -1,3 +1,4 @@
+--in this query we look for and specific company and count the main lost reasons for a salesman
 SELECT 
     pipedrive_deals.lost_reason,
     count(pipedrive_deals.lost_reason) as total
@@ -5,12 +6,12 @@ FROM
     pipedrive_deals
         INNER JOIN
     vendedores ON vendedores.ref_user_pipedrive = pipedrive_deals.user_id
-    and vendedores.salesfarm_ref_id = pipedrive_deals.salesfarm_ref_id
+    and vendedores.company_ref_id = pipedrive_deals.company_ref_id
 		inner join pipedrive_stages on pipedrive_stages.stage_id = pipedrive_deals.stage_id
-        and pipedrive_stages.salesfarm_ref_id = pipedrive_deals.salesfarm_ref_id
+        and pipedrive_stages.company_ref_id = pipedrive_deals.company_ref_id
 where 
 	pipedrive_deals.status = "lost"
-    and pipedrive_deals.salesfarm_ref_id = 1860
-    and vendedores.ref_user_pipedrive = 11814605
+    and pipedrive_deals.company_ref_id = --insert id
+    and vendedores.ref_user_pipedrive = --insert salesman id
     and pipedrive_stages.pipeline_id = 1
     group by pipedrive_deals.lost_reason
